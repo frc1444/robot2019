a3816a1d555556bf85012da84db042be08ae3e90
Joshua Shannon <retrodaredevil@gmail.com>
2019-01-22 19:19:39 -0600

    Perfected TurnToOrientation, scales trigger input, added MutableValueMapSendable

---
+++ src/main/java/com/first1444/frc/util/valuemap/sendable/MutableValueMapSendable.java
@@ -0,0 +1,4 @@
+package com.first1444.frc.util.valuemap.sendable;
+
+public class MutableValueMapSendable {
+}
--- src/main/java/com/first1444/frc/util/valuemap/sendable/ValueSendables.java
+++
@@ -1,8 +0,0 @@
-package com.first1444.frc.util.valuemap.sendable;
-
-import edu.wpi.first.wpilibj.Sendable;
-
-public final class ValueSendables {
-	private ValueSendables(){ throw new UnsupportedOperationException(); }
-
-}
